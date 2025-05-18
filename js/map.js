639884b2b55421e918feab1688d52d1d7da0522e
decvoz <decvozza@gmail.com>
2025-05-18 12:34:44 +1000

    Update map.js

--- js/map.js
+++ js/map.js
@@ -13,7 +13,7 @@ fetch('geojson/publictoilets_clean.geojson')
   .then(data => {
     const toiletLayer = L.geoJSON(data, {
       pointToLayer: (feature, latlng) => {
-        const iconHTML = `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="white" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" style="background: rgba(0, 77, 153, 0.8); border-radius: 50%; padding: 2px; width: 32px; height: 32px;"><path d="M7 12h13a1 1 0 0 1 1 1 5 5 0 0 1-5 5h-.598a.5.5 0 0 0-.424.765l1.544 2.47a.5.5 0 0 1-.424.765H5.402a.5.5 0 0 1-.424-.765L7 18"/><path d="M8 18a5 5 0 0 1-5-5V4a2 2 0 0 1 2-2h8a2 2 0 0 1 2 2v8"/></svg>`;
+        const iconHTML = `<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="#ffffff" stroke-width="1" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-toilet-icon lucide-toilet"><path d="M7 12h13a1 1 0 0 1 1 1 5 5 0 0 1-5 5h-.598a.5.5 0 0 0-.424.765l1.544 2.47a.5.5 0 0 1-.424.765H5.402a.5.5 0 0 1-.424-.765L7 18"/><path d="M8 18a5 5 0 0 1-5-5V4a2 2 0 0 1 2-2h8a2 2 0 0 1 2 2v8"/></svg>`;
         const divIcon = L.divIcon({
           className: '',
           html: iconHTML,
